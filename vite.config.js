666a58b5ada79684a183a0e3778a02b6cb94834b
Dhana-Sree-02 <dhanasree0238@gmail.com>
2026-03-14 10:37:31 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server:{port:30321,},
-  base:'/R-project2/',
+  base:'/FEDF-employee-info-viewer/',
 
 })
